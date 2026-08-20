279730f7773d7d24720683bf603b3dd12306b94c
ARYAN RAO <123591598+Aryanrao0@users.noreply.github.com>
2026-08-21 03:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (66.96%) - LeetSync

--- 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
+++ 3347-distribute-elements-into-two-arrays-i/distribute-elements-into-two-arrays-i.cpp
@@ -1,19 +1,43 @@
 class Solution {
 public:
     vector<int> resultArray(vector<int>& nums) {
-        vector<int>arr1 = {nums[0]};
-        vector<int>arr2 = {nums[1]};
+        // vector<int>arr1 = {nums[0]};
+        // vector<int>arr2 = {nums[1]};
 
-        for(int i=2;i<nums.size();i++){
-            if(arr1.back()>arr2.back()){
+        // for(int i=2;i<nums.size();i++){
+        //     if(arr1.back()>arr2.back()){
+        //         arr1.push_back(nums[i]);
+        //     }
+        //     else{
+        //         arr2.push_back(nums[i]);
+        //     }
+        // }
+
+        // approch 2
+
+        vector<int>arr1;
+        vector<int>arr2;
+
+        arr1.push_back(nums[0]);
+        arr2.push_back(nums[1]);
+
+        int last1 = nums[0];
+        int last2 = nums[1];
+
+        int i=2;
+        while(i<nums.size()){
+            if(last1>last2){
                 arr1.push_back(nums[i]);
+                last1 = nums[i];
             }
             else{
                 arr2.push_back(nums[i]);
+                last2 = nums[i];
             }
+            i++;
         }
 
-        for(int x : arr2){
+        for(auto x : arr2){
             arr1.push_back(x);
         }
         return arr1;
