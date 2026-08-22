65ec52762d1083cb093c7b877cc4d64ef40e7a3f
ARYAN RAO <123591598+Aryanrao0@users.noreply.github.com>
2026-08-22 21:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 14.8 MB (85.77%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,33 +11,18 @@
  */
 class Solution {
 public:
+    void preOrder(TreeNode* root,int level ,vector<int>& ans){
+        if(root==NULL) return ;
+
+        if(ans.size()<level) ans.push_back(root->val);
+        preOrder(root->right,level+1,ans);
+        preOrder(root->left,level+1,ans);
+
+    }
     vector<int> rightSideView(TreeNode* root) {
-        if(root==NULL) return {};
-
+        // DFS 
         vector<int> ans;
-        queue<TreeNode* > q;
-        q.push(root);
-
-        while(!q.empty()){
-            TreeNode* node = NULL;
-            int n = q.size();
-
-            while(n--){ 
-                node = q.front();
-                q.pop();
-
-                if(node->left!=NULL){
-                    q.push(node->left);
-                }
-                if(node->right!=NULL){
-                    q.push(node->right);
-                }
-
-            }
-            ans.push_back(node->val);
-        }
-
+        preOrder(root,1,ans);
         return ans;
-
     }
 };
